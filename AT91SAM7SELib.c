6ef502edbc174e1083d3f03170b9f6941c9da008
thewisenerd <thewisenerd@protonmail.com>
2015-11-14 21:56:02 +0530

    initial commit
    a messy place to start, but still...
    
    Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

---
+++ AT91SAM7SELib.c
@@ -0,0 +1,233 @@
+/* AT91SAM7SELib.c
+ *
+ * Copyright (c) 2015, thewisenerd <thewisenerd@protonmail.com>
+ *
+ * Proprietary, and Confidential till futher notice.
+ *
+ */
+
+#include "AT91SAM7SELib.h"
+
+void delay_ms(unsigned int n)
+{
+  unsigned int i = MY_OSC_2 * n;
+  while(i)i--;
+}
+
+void enable_led(unsigned int LED)
+{
+  if (LED == AT91B_POWERLED)
+    AT91C_BASE_PIOA->PIO_SODR = LED;
+  else
+    AT91C_BASE_PIOA->PIO_CODR = LED;
+}
+
+void disable_led(unsigned int LED)
+{
+  if (LED == AT91B_POWERLED)
+    AT91C_BASE_PIOA->PIO_CODR = LED;
+  else
+    AT91C_BASE_PIOA->PIO_SODR = LED;
+}
+
+void blinkenlights(uint8_t LED, size_t delay, size_t n)
+{
+  while(n--) {
+    enable_led(LED);
+    delay_ms(delay);
+    disable_led(LED);
+    delay_ms(delay);
+  }
+}
+
+/* stdlib overwrites */
+signed int fgetc(FILE *pStream)
+{
+  if (pStream == stdin) {
+    return USART_read(AT91C_BASE_US1);
+  } else {
+    return EOF;
+  }
+}
+
+signed int fputc(signed int c, FILE *pStream)
+{
+  if ((pStream == stdout) || (pStream == stderr)) {
+    USART_write(AT91C_BASE_US1, c);
+    return c;
+  } else {
+    return EOF;
+  }
+}
+
+signed int fputs(const char *pStr, FILE *pStream)
+{
+  signed int num = 0;
+  while (*pStr != 0) {
+    if (fputc(*pStr, pStream) == -1) {
+      return -1;
+    }
+    num++;
+    pStr++;
+  }
+  return num;
+}
+
+char* fgets(char *str, int size, FILE *pStream)
+{
+  if (pStream == stdin) {
+    while (size--) {
+      *str++ = USART_read(AT91C_BASE_US1);
+    }
+    return str;
+  } else {
+    return NULL;
+  }
+}
+
+int ferror(FILE *f)
+{
+  return EOF;
+}
+/* stdlib overwrites (end) */
+
+/* pmc */
+void PMC_EnablePeripheral( uint8_t source )
+{
+  if (source < 32)
+    AT91C_BASE_PMC->PMC_PCER = 1 << source;
+}
+
+void PMC_DisablePeripheral( uint8_t source )
+{
+  if ((AT91C_BASE_PMC->PMC_PCSR & (1 << source)) == (1 << source))
+    AT91C_BASE_PMC->PMC_PCDR = 1 << source;
+}
+void PMC_DisableInterrupts(AT91S_PIO *pio, unsigned int filter)
+{
+  pio->PIO_IDR = filter; 
+}
+
+void PMC_EnableInterrupts(AT91S_PIO *pio, unsigned int filter)
+{
+  pio->PIO_IER = filter;
+}
+/* pmc (end) */
+
+/* AIC */
+void AIC_ConfigureIT(uint8_t source, uint8_t mode, void (*handler)(void))
+{
+  // Disable the interrupt first
+  AT91C_BASE_AIC->AIC_IDCR = 1 << source;
+
+  // Configure mode and handler
+  AT91C_BASE_AIC->AIC_SMR[source] = mode;
+  AT91C_BASE_AIC->AIC_SVR[source] = (unsigned int) handler;
+
+  // Clear interrupt
+  AT91C_BASE_AIC->AIC_ICCR = 1 << source;
+}
+
+void AIC_EnableIT(uint8_t source)
+{
+  AT91C_BASE_AIC->AIC_IECR = 1 << source;
+}
+
+void AIC_DisableIT(uint8_t source)
+{
+  AT91C_BASE_AIC->AIC_IDCR = 1 << source;
+}
+
+void AIC_Init( void )
+{
+  // disable all interrupts
+  AT91C_BASE_AIC->AIC_IDCR = 0xFFFFFFFF;
+  // clear all interrupts
+  AT91C_BASE_AIC->AIC_ICCR = 0xFFFFFFFF;
+}
+/* AIC (end) */
+
+/* USART */
+void USART_Configure(AT91S_USART *usart, uint mode, uint baudrate, uint masterClock) {
+  // Reset and disable receiver & transmitter
+  usart->US_CR = AT91C_US_RSTRX | AT91C_US_RSTTX | AT91C_US_RXDIS | AT91C_US_TXDIS;
+
+  // Configure mode
+  usart->US_MR = mode;
+
+  // Configure baudrate
+  usart->US_BRGR = (masterClock / baudrate) / 16;
+}
+
+uint8_t USART_read(AT91S_USART *usart)
+{
+  enable_led(USART_RX_LED);
+  while ((usart->US_CSR & AT91C_US_RXRDY) == 0); // while !rxrdy, wait.
+  disable_led(USART_RX_LED);
+  return usart->US_RHR;
+}
+
+size_t USART_write(AT91S_USART *usart, uint8_t c)
+{
+  enable_led(USART_TX_LED);
+  while ((usart->US_CSR & AT91C_US_TXRDY) == 0); // while !txrdy, wait.
+
+  usart->US_THR = c;
+  while ((usart->US_CSR & AT91C_US_TXRDY) == 0); // while !txrdy, wait.
+  disable_led(USART_TX_LED);
+  return 1;
+}
+
+void USART_SetTransmitterEnabled(AT91S_USART *usart, uint8_t enabled)
+{
+  if (enabled) {
+    usart->US_CR = AT91C_US_TXEN;
+  } else {
+    usart->US_CR = AT91C_US_TXDIS;
+  }
+}
+
+void USART_SetReceiverEnabled(AT91S_USART *usart, uint8_t enabled)
+{
+  if (enabled) {
+    usart->US_CR = AT91C_US_RXEN;
+  } else {
+    usart->US_CR = AT91C_US_RXDIS;
+  }
+}
+
+void USART_SetRecieverTimeout(AT91S_USART *usart, unsigned int timeout) {
+  usart->US_RTOR = timeout;
+}
+
+void USART_SetTransmitterTimeguard(AT91S_USART *usart, unsigned int timeguard) {
+  usart->US_TTGR = timeguard;
+}
+
+void USART_DisableInterrupts(AT91S_USART *usart, unsigned int filter)
+{
+  usart->US_IDR = filter; 
+}
+
+void USART_EnableInterrupts(AT91S_USART *usart, unsigned int filter)
+{
+  usart->US_IER = filter; 
+}
+/* USART (end) */
+
+/* rtc */
+unsigned int getRTCVal( void ) {
+  return AT91C_BASE_RTTC->RTTC_RTVR;
+}
+
+void resetRTC( uint8_t resolution, unsigned int filter ) {
+  // reset with 1s resolution
+  AT91C_BASE_RTTC->RTTC_RTMR = (1 << resolution) | AT91C_RTTC_RTTRST;
+}
+
+void setRTCAlarm ( unsigned int time )
+{
+  AT91C_BASE_RTTC->RTTC_RTAR = time - 1;
+  //pRtt->RTTC_RTAR = - 1;   
+}   
+/* rtc (end) */
---
+++ AT91SAM7SELib.h
@@ -0,0 +1,66 @@
+/* AT91SAM7SELib.h
+ *
+ * Copyright (c) 2015, thewisenerd <thewisenerd@protonmail.com>
+ *
+ * Proprietary, and Confidential till futher notice.
+ *
+ */
+
+#include <AT91SAM7SE512.H>              /* AT91SAM7SE512 definitions          */
+#include "AT91SAM7SE-EK.h"           /* AT91SAM7SE-EK board definitions    */
+
+#include <stdio.h>
+
+#define uint8_t unsigned char
+#define uint    unsigned int
+
+#define BOARD_MCK 48000000
+#define MY_OSC_2  5000      // iterations for 1ms delay (approx)
+
+#define PIN_USART0_RXD  (1 << 5)
+#define PIN_USART0_TXD  (1 << 6)
+#define PINS_USART0     (PIN_USART0_RXD | PIN_USART0_TXD)
+
+#define PIN_USART1_RXD  (1 << 21)
+#define PIN_USART1_TXD  (1 << 22)
+#define PINS_USART1     (PIN_USART1_RXD | PIN_USART1_TXD)
+
+#define USART_RX_LED AT91B_LED2
+#define USART_TX_LED AT91B_LED1
+
+void delay_ms(unsigned int n);
+
+void enable_led(unsigned int LED);
+void disable_led(unsigned int LED);
+void blinkenlights(uint8_t LED, size_t delay, size_t n);
+
+/* pmc */
+void PMC_EnablePeripheral( uint8_t source );
+void PMC_DisablePeripheral( uint8_t source );
+void PMC_DisableInterrupts(AT91S_PIO *pio, unsigned int filter);
+void PMC_EnableInterrupts(AT91S_PIO *pio, unsigned int filter);
+/* pmc (end) */
+
+/* AIC */
+void AIC_ConfigureIT(uint8_t source, uint8_t mode, void (*handler)(void));
+void AIC_EnableIT(uint8_t source);
+void AIC_DisableIT(uint8_t source);
+void AIC_Init( void );
+/* AIC (end) */
+
+/* u(s)art functions */
+void    USART_Configure(AT91S_USART *usart, uint mode, uint baudrate, uint masterClock);
+uint8_t USART_read(AT91S_USART *usart);
+size_t  USART_write(AT91S_USART *usart, uint8_t c);
+void    USART_SetTransmitterEnabled(AT91S_USART *usart, uint8_t enabled);
+void    USART_SetReceiverEnabled(AT91S_USART *usart, uint8_t enabled);
+void    USART_SetRecieverTimeout(AT91S_USART *usart, unsigned int timeout);
+void    USART_SetTransmitterTimeguard(AT91S_USART *usart, unsigned int timeguard);
+void    USART_DisableInterrupts(AT91S_USART *usart, unsigned int filter);
+void    USART_EnableInterrupts(AT91S_USART *usart, unsigned int filter);
+/* u(s)art functions (end) */
+
+/* rtc */
+unsigned int getRTCVal( void );
+void         resetRTC( uint8_t resolution, unsigned int filter );
+/* rtc (end) */
